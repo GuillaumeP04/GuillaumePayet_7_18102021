4693a915326f59cdc9d80ae2cc0ede4ba0dada50
Guillaume Payet <guillaumepayet04@gmail.com>
2021-10-19 18:12:55 +0400

    Dropdown menu with items done

---
+++ javascript/main.js
@@ -0,0 +1,90 @@
+displayRecipe();
+displayIngredients();
+displayAppareil();
+displayUstensiles();
+
+function displayRecipe() {
+    let html = " ";
+    recipes.forEach(recipe => {
+        html += renderRecipe(recipe);
+    })
+    document.getElementById("main--wrapper").innerHTML = html;
+}
+
+function renderRecipe(recipe) {
+    let ingredients = " ";
+    recipe.ingredients.forEach(item => {
+        if (item.quantity == undefined) {
+            ingredients += `<p><strong>${item.ingredient}</strong></p>`;
+        } else if (item.unit == undefined) {
+            ingredients += `<p><strong>${item.ingredient}</strong>: ${item.quantity}</p>`;
+        } else {
+            ingredients += `<p><strong>${item.ingredient}</strong>: ${item.quantity}${item.unit}</p>`;
+        }
+    })
+
+    return `<div class="recipe--wrapper">
+                <img src="/images/grey.jpg" alt="" class="recipe--image">
+                <div class="recipe--description">
+                    <div class="title--wrapper">
+                        <p class="title">${recipe.name}</p>
+                        <p class="time"><strong><i class="far fa-clock"></i> ${recipe.time} min</strong></p>
+                    </div>
+                    <div class="div--wrapper">
+                        <div class="ingredients--wrapper">
+                            ${ingredients}
+                        </div>
+                        <div class="description--wrapper">
+                            <p>${recipe.description}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
+
+function displayIngredients() {
+    let ingredientList = new Set();
+    let html = " ";
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(item => {
+            if (!ingredientList.has(item.ingredient)) {
+                ingredientList.add(item.ingredient);
+            }
+        })
+    })
+    ingredientList.forEach(item => {
+        html += `<a class="dropdown--content filter">${item}</a>`
+    })
+    document.querySelector("#ingredients").innerHTML = html;
+}
+
+function displayAppareil() {
+    let appareilList = new Set();
+    let html = " ";
+    recipes.forEach(recipe => {
+        if (!appareilList.has(recipe.appliance)) {
+            appareilList.add(recipe.appliance);
+        }
+    })
+    appareilList.forEach(item => {
+        html += `<a class="dropdown--content filter">${item}</a>`
+    })
+    document.querySelector("#appareil").innerHTML = html;
+}
+
+function displayUstensiles() {
+    let ustensilesList = new Set();
+    let html = " ";
+    recipes.forEach(recipe => {
+        recipe.ustensils.forEach(items => {
+            let item = items.charAt(0).toUpperCase() + items.substr(1);
+            if (!ustensilesList.has(item)) {
+                ustensilesList.add(item);
+            }
+        })
+    })
+    ustensilesList.forEach(item => {
+        html += `<a class="dropdown--content filter">${item}</a>`
+    })
+    document.querySelector("#ustensiles").innerHTML = html;
+}
